917108380954b39fc0ef0cb126b5b1c13add83b1
Pranav Sinha <pran.sinha1.0@gmail.com>
2020-10-05 20:29:31 +0530

    debugged db creation

--- librarymanagement/__init__.py
+++ librarymanagement/__init__.py
@@ -1,9 +1,9 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
 
 app=Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///LMS.db"
+app.config['SECRET_KEY']="Hello"
 db = SQLAlchemy(app)
 
 from librarymanagement import routes
--- librarymanagement/models.py
+++ librarymanagement/models.py
@@ -1,6 +1,9 @@
 from librarymanagement import db
+from flask_login import UserMixin
+from datetime import datetime
 
-class member(db.Model):
+class member(UserMixin,db.Model):
+    __tablename__="member"
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(50), nullable = False)
     email = db.Column(db.Text, unique = True, nullable = False)
@@ -8,19 +11,21 @@ class member(db.Model):
     address = db.Column(db.Text, nullable = False)
     curr_no_books = db.Column(db.Integer, default = 0)
     mobiles = db.relationship("memberMobile", backref = "member", lazy = True)
-    issueInfo  = db.relationship("issueInfo", backref = "issueInfo", lazy = True) 
+    member_issueInfo  = db.relationship("issueInfo", backref = "member", lazy = True) 
 
     def __repr__(self):
         return f"member('{self.id}', '{self.name}', '{self.email}')"
 
-class memberMobile(db.Model):
+class memberMobile(UserMixin,db.Model):
+    __tablename__="membermobile"
     member_id = db.Column(db.Integer, db.ForeignKey("member.id"), primary_key = True, nullable = False)
     mobile = db.Column(db.Integer, unique = True, nullable = False)
 
     def __repr__(self):
         return f"memberMobile('{self.member_id}', '{self.mobile}')"
 
-class librarian(db.Model):
+class librarian(UserMixin,db.Model):
+    __tablename__="librarian"
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(50), nullable = False)
     email = db.Column(db.Text, unique = True, nullable = False)
@@ -30,59 +35,65 @@ class librarian(db.Model):
     shift_from = db.Column(db.DateTime, nullable = False)
     shift_till = db.Column(db.DateTime, nullable = False)
     mobiles = db.relationship("librarianMobile", backref = "librarian", lazy = True)
-    issueInfo  = db.relationship("issueInfo", backref = "issueInfo", lazy = True) 
+    librarian_issueInfo  = db.relationship("issueInfo", backref = "librarian",lazy=True) 
 
     def __repr__(self):
         return f"librarian('{self.id}', '{self.name}', '{self.doj}')"
 
-class librarianMobile(db.Model):
-    lib_id = db.Column(db.Integer, db.ForeignKey("librarian.id"), primray_key = True, nullable = False)
+class librarianMobile(UserMixin,db.Model):
+    __tablename__="librarianmobile"
+    lib_id = db.Column(db.Integer, db.ForeignKey("librarian.id"), primary_key=True,nullable = False)
     mobile = db.Column(db.Integer, unique = True, nullable = False)
 
     def __repr__(self):
         return f"librarianMobile('{self.lib_id}', '{self.mobile}')"
 
-class book(db.Model):
+class book(UserMixin,db.Model):
+    __tablename__="book"
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(100), nullable = False)
     publisher = db.Column(db.String(50), nullable = False)
     isbn = db.Column(db.Integer, unique = True, nullable = True) #May be nullable if the book is old
     genres = db.relationship("bookGenre", backref = "genre", lazy = True)
     authors = db.relationship("bookAuthor", backref = "author", lazy = True)
-    issueInfo  = db.relationship("issueInfo", backref = "issueInfo", lazy = True) 
-    issueStatus = issueInfo  = db.relationship("issuedOrReturned", backref = "issueInfo", lazy = True) 
+    book_issueInfo  = db.relationship("issueInfo", backref = "book", lazy = True) 
+    issueStatus = db.relationship("issuedOrReturned", backref = "book", lazy = True) 
 
     def __repr__(self):
         return f"book('{self.id}', '{self.name}', '{self.publisher}', '{self.isbn}')"
 
-class bookGenre(db.Model):
-    book_id = db.Column(db.Integer, db.ForeignKey("book.id"), nullable = False)
+class bookGenre(UserMixin,db.Model):
+    __tablename__="bookgenre"
+    book_id = db.Column(db.Integer, db.ForeignKey("book.id"), primary_key=True, nullable = False)
     genre = db.Column(db.String(50), nullable = False)
 
     def __repr__(self):
         return f"bookGenre('{self.book_id}', '{self.genre}')"
 
-class bookAuthor(db.Model):
-    book_id = db.Column(db.Integer, db.ForeignKey("book.id"), nullable = False)
+class bookAuthor(UserMixin,db.Model):
+    __tablename__="bookauthor"
+    book_id = db.Column(db.Integer, db.ForeignKey("book.id"), primary_key=True, nullable = False)
     author = db.Column(db.String(50), nullable = False)
 
     def __repr__(self):
         return f"bookAuthor('{self.book_id}', '{self.author}')"
 
-class issueInfo(db.Model):
+class issueInfo(UserMixin,db.Model):
+    __tablename__="issueinfo"
     id = db.Column(db.Integer, primary_key = True)
     date = db.Column(db.DateTime, nullable = False, default = datetime.utcnow) #Check if default is required here
     member_id = db.Column(db.Integer, db.ForeignKey("member.id"), nullable = False)
     book_id = db.Column(db.Integer, db.ForeignKey("book.id"), nullable = False)
     lib_id = db.Column(db.Integer, db.ForeignKey("librarian.id"), nullable = False)
-    issueStatus = db.relationship("issuedOrReturned", backref = "issueInfo", lazy = True)
+    issueStatus = db.relationship("issuedOrReturned", backref = "issueinfo", lazy = True)
 
     def __repr__(self):
         return f"issueInfo('{self.id}', '{self.date}', '{self.member_id}', '{self.book_id}', '{self.lib_id}')"
 
-class issuedOrReturned(db.Model):
-    issue_id = db.Column(db.Integer, db.ForeignKey("issueInfo.id"), nullable = False)
-    book_id = db.Column(db.Integer, db.ForeignKey("book.id"), nullable = False)
+class issuedOrReturned(UserMixin,db.Model):
+    __tablename__="issuedorreturned"
+    issue_id = db.Column(db.Integer, db.ForeignKey("issueinfo.id"), primary_key=True, nullable = False)
+    book_id = db.Column(db.Integer, db.ForeignKey("book.id"), primary_key=True, nullable = False)
     return_date = db.Column(db.DateTime, nullable = False, default = datetime.utcnow) #Check if default is required here
 
     def __repr__(self):
