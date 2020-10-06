556bdba602b77e103b6424129ee3d5149704e809
Pranav Sinha <pran.sinha1.0@gmail.com>
2020-10-06 19:52:33 +0530

    Debugged opacity

--- librarymanagement/routes.py
+++ librarymanagement/routes.py
@@ -1,6 +1,6 @@
 from librarymanagement import app
 from flask import render_template
 
-@app.route("/login",methods=["GET","POST"])
+@app.route("/",methods=["GET","POST"])
 def login():
     return render_template("login.html")
